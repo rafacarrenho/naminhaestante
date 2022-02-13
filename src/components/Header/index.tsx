a9c8aeb7cc4b53c1135ba6695a247afa86b722c4
Rafael Carrenho <rafael.carrenho@hotmail.com>
2022-02-13 16:57:19 -0300

    feat: Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles";
+
+export const Header = () => {
+  return (
+    <S.Wrapper>
+      <S.Container>Home</S.Container>
+    </S.Wrapper>
+  );
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+`;
+
+export const Container = styled.div`
+  width: 1200px;
+  max-width: 100%;
+`;
